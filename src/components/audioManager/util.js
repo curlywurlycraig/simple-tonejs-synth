171c35310f54798343a65d6403db2cd7c774e4de
Craig Wilkinson <craig@dangerfarms.com>
2018-01-05 13:33:07 +0000

    Construct new nodes within the racks

--- src/components/audioManager/util.js
+++ src/components/audioManager/util.js
@@ -3,8 +3,57 @@ import {convertNoteToFrequency} from '../../utils';
 /**
  * Given an audio routing graph and the json representation of an
  * audio routing graph, update the routing graph in place.
+ *
+ * TODO: Remove old and create new racks, and units.
+ * TODO: Substantially test the behaviour of this function
+ *       when it is known how to use Web Audio in Jest tests
  */
-export function updateRoutingGraph(routingGraph, audioState) {
+export function updateRoutingGraph(actualGraph, reprGraph) {
+  updateNodeToRepresentation(actualGraph.outputNode, reprGraph.outputNode);
+
+  reprGraph.racks.forEach(reprRack => {
+    let actualRack = actualGraph.racks.find(actualRackCandidate => {
+      actualRackCandidate.id === reprRack.id;
+    });
+
+    if (!actualRack) {
+      actualRack = {
+        id: reprRack.id,
+        units: []
+      };
+      actualGraph.racks[actualRack.id] = actualRack;
+    }
+
+    reprRack.units.forEach(reprUnit => {
+      let actualUnit = actualRack.units.find(actualUnitCandidate => {
+        actualUnitCandidate.id === reprUnit.id;
+      });
+
+      if (!actualUnit) {
+        actualUnit = {
+          id: reprUnit.id,
+          nodes: {},
+        };
+        actualRack.units[actualUnit.id] = actualUnit;
+      }
+
+      Object.keys(reprUnit.nodes).forEach(reprNodeKey => {
+        // TODO: What if the node we want to connect to doesn't exist?
+        // Answer: we should construct all the nodes before we perform the connections.
+
+        // TODO: There's a better way to iterate over key/value pairs of an object.
+        // I'm sure MDN has something to say about it.
+        const reprNode = reprUnit.nodes[reprNodeKey];
+
+        if (!actualUnit.nodes[reprNodeKey]) {
+          actualUnit.nodes[reprNodeKey] = newNodeFromRepresentation(reprNode);
+        }
+      })
+    })
+  });
+}
+
+function updateNodeToRepresentation(routingGraphNode, nodeRepresentation) {
   if (routingGraph.outputNode._node === undefined) {
     routingGraph.outputNode = {...audioState.outputNode};
     routingGraph.outputNode._node = newNodeFromRepresentation(
@@ -25,7 +74,7 @@ function newNodeFromRepresentation(nodeRepr, context) {
 
 function updateNodeWithParams(node, params) {
   for (key in params) {
-    node[key] = params[key];
+    node[key].value = params[key];
   }
 
   return node;
